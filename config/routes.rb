8b8c17b5a9d210af884ab61e5bc11903cb63e57d
Tony Pizicatto <13diego13@mail.ru>
2021-02-04 16:52:06 +0300

    create users controller and routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
+  get 'users/new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :task
 
 
   get 'dashboard/index'
   get 'about' => 'dashboard#about'
+  get 'signup'  => 'users#new'
 
 
 
